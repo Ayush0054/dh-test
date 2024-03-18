65415160a0236b56e4df3e55f6f5696069962974
DevRico003 <carteciano.enrico@gmail.com>
2024-03-18 18:41:34 +0100

    first commit

--- app/(marketing)/components/site-footer.tsx
+++ app/(marketing)/components/site-footer.tsx
@@ -12,16 +12,16 @@ export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <Icons.logo />
           <p className="text-center text-sm leading-loose md:text-left">
-            Built by{" "}
+            
             <a
               href={siteConfig.links.author}
               target="_blank"
               rel="noreferrer"
               className="font-medium underline underline-offset-4"
             >
-              mvpfoundry.com
+              
             </a>
-            . Contact me us{" "}
+            Contact us{" "}
             <a
               href={siteConfig.links.linkedin}
               target="_blank"
